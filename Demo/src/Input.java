b8d51315a8dbe72bb32ba5e789fad378a085c33a
darsh <darsh@DESKTOP-MG00CVE>
2023-07-20 23:36:33 +0530

    Third Commit

---
+++ Demo/src/Input.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Input {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the string :");
+		String str=sc.nextLine();
+		System.out.println("Entered String is "+str);
+		sc.close();
+		
+
+	}
+
+}
